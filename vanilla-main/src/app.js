a3015808c388b220e943ec28cacba7fd04d401fe
Reagan Bohan <xbjfk.github@gmail.com>
2022-02-19 23:59:14 +1300

    ordering bar

--- vanilla-main/src/app.js
+++ vanilla-main/src/app.js
@@ -1,16 +1,32 @@
 const API_URL = "http://127.0.0.1:5000/menu";
 
-/*
 fetch(API_URL).then(function(response) {
 	response.text().then(function(text) {
 		console.log(text);
 	})
 })
-*/
 
 
 
-async function getMenuItems(url) {
-	const response = await fetch(url);
-	var data = await response.json();
+demo = {
+	"to": "Staffroom",
+	"name": "ms teacher",
+	"items": [
+		{
+			"id": 1,
+			"size": 1,
+			"count": 3
+		},
+		{
+			"id": 2,
+			"size": 0,
+			"count": 6
+		}
+	]
 }
+
+function newOrder(order) {
+	
+}
+
+newOrder(demo)
