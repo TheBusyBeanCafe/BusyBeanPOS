705635d2e12e10fe4fb665ee74bf963ea4cc719d
tim <pdt.xie@gmail.com>
2022-02-19 23:45:56 +1300

    working buttons

--- vanilla-main/src/app.js
+++ vanilla-main/src/app.js
@@ -1,16 +1,57 @@
 const API_URL = "http://127.0.0.1:5000/menu";
 
-/*
-fetch(API_URL).then(function(response) {
-	response.text().then(function(text) {
-		console.log(text);
-	})
-})
-*/
-
-
-
 async function getMenuItems(url) {
 	const response = await fetch(url);
 	var data = await response.json();
+
+	displayData(data);
 }
+
+
+function displayData(data) {
+	let tab;
+	
+	data.forEach(function(element) { 
+		console.log(element.id);
+		console.log(element.short_name);
+
+		tab += `
+			<div class="grid-item">
+				<button id="button${element.id}" type="button" onclick="orderCoffee(${element})">${element.short_name}</button>
+			</div>
+			`;	 
+	})
+
+	console.log(tab);
+
+	document.getElementById("grid-container").innerHTML = tab;
+}
+
+// let orderedCoffees = [];
+
+getMenuItems(API_URL);
+
+function orderCoffee(item) {
+	console.log(item.long_name);
+
+	// orderedCoffees.push(item.long_name);
+
+	// let display;
+
+	// for (const i in item) {
+	// 	console.log(i);
+	// }
+
+	// orderedCoffees.forEach(function(element) { 
+	// 	console.log(element);
+
+	// 	display += `
+	// 		<p>${element}</p>
+	// 	`;
+	// })
+
+	// document.getElementById("order-list").innerHTML = display;
+}
+
+
+
