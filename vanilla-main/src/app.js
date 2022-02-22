8f9270dc692994e84a41f8a42076b9222ee81df2
tim <pdt.xie@gmail.com>
2022-02-23 00:13:57 +1300

    many changes to UI, linked buttons, added endshift buttons

--- vanilla-main/src/app.js
+++ vanilla-main/src/app.js
@@ -17,12 +17,142 @@ demo = {
 	]
 }
 
+var menu, drink_addons, modal;
 
+let orderedCoffees = [];
+
+
+
+
+function getOrderSubText() {
+	var currentDate = new Date();
+	var hour = currentDate.getHours();
+	var day = currentDate.getDay();
+	
+	day = 4;
+	hour = 8;
+	
+	var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][day]
+	
+	
+	function getShiftName(hour) {
+		if (hour >= 7 && hour <= 9) {
+			return "Morning Shift";
+		} else if (hour >= 12 && hour <= 14) {
+			return "Lunch Shift";
+		} else {
+			return "";
+		}
+	}
+	
+	return weekday + ", " + getShiftName(hour);
+}
+
+function postCoffee(coffee) {
+	console.log(coffee)
+	fetch(API_URL + "transactions", {
+		method: "POST",
+		headers: {'Content-Type': 'application/json'},
+		body: JSON.stringify(coffee)
+	})
+}
+
+function addCoffee(idx) {
+	item = menu[idx]
+	if (item.is_drink) {
+		modal.style.display = "block"
+		ok = function(iidx) {
+			modal.style.display = 'none'
+			object = {
+				
+				
+				index: iidx,
+				addons: [],
+				large: document.getElementById("large").checked,
+				count: 1 // TODO
+			}
+			drink_addons.forEach(function(element, index) {
+				switch (element.type) {
+					case "toggle":
+					object.addons[index] = document.getElementById(element.name).checked
+					break;
+					case "choice":
+					var choice
+					[].slice.call(document.getElementById(element.name).getElementsByTagName("input")).every( (el, idx) => {
+						if (el.checked) {
+							choice = idx
+							return false
+						}
+						return true
+					})
+					object.addons[index] = choice
+					break;
+					case "count":
+					object.addons[index] = []
+					for (idx in element.choices) {
+						object.addons[index][idx] = document.getElementById(`${element.name}choice${idx}`).valueAsNumber
+					}
+					
+				}
+			})
+			postCoffee(object);
+			
+			orderedCoffees.push(object);
+			updCurTransList()
+		}
+		var options = ""
+		options+="<h3>Large</h3>"
+		options+="<input id=\"large\" type=\"checkbox\">"
+		for (const option of drink_addons) {
+			
+			options+=`<h3>${option.name}</h3>`
+			switch (option.type) {
+				case "toggle":
+				options+=`<input id="${option.name}" type=\"checkbox\">`
+				break;
+				case "choice":
+				options+=`<div id="${option.name}">`
+				option.choices.forEach(function(element, index) {
+					options+=`
+					<input type="radio" id="${option.name}choice${index}"
+					name="${option.name}" ${index === option.default ? "checked" : ""}>
+					<label for="${option.name}choice${index}">${element.name}</label>
+					`
+				})
+				options+="</div>"
+				break;
+				case "count":
+				option.choices.forEach(function(element, index) {
+					options+=`
+					<input type="number" value="0" id="${option.name}choice${index}">
+					<label for="${option.name}choice${index}">${element.name}</label>
+					`
+				})
+			}
+		}
+		modal.innerHTML = `
+		<div class="modal-content">
+		<h2>${item.long_name}</h2>
+		${options}
+		<div class="modal-buttons">
+		<button onclick="modal.style.display = 'none'" type="button">Cancel</button>
+		<button onclick="ok(${idx})" type="button">OK</button>
+		</div>
+		</div>
+		`
+	} else {
+		pushCoffee(item);
+		
+		orderedCoffees.push({index: idx});
+		updCurTransList()
+	}
+	
+}
 
 function getCurrentDate() {
 	var currentDate = new Date();
 	var date = currentDate.getHours();
-
+	
 	if (date > 7 && date < 9) {
 		return "Good Morning, "
 	} else if (date > 12 && date < 2) {
@@ -37,88 +167,98 @@ function getCurrentName() {
 	console.log(data);
 }
 
-
 async function asyncFetch(url) {
 	const response = await fetch(url);
 	json = await response.json()
-
+	
 	return json
 }
-
-
 
 function getIntroText() {
 	var introText = "";
 	introText += getCurrentDate()
-
+	
 	getCurrentName();
 }
-
-
-window.addEventListener("load", getCurrentDate);
-
-
-function newOrder(order) {
-	
-}
-
-
-
-
-
-newOrder(demo)
-
-var menu
-var drink_addons
 
 async function getMenuItems(url) {
 	const response = await fetch(url);
 	json = await response.json();
 	menu = json["menu"]
 	drink_addons = json["drink_addons"]
-
+	
 	displayData(menu);
 }
 
-
 function displayData(data) {
-	if ( document.readyState == 'complete' ) {
+	function addClickListener() {
+		for (let i = 0; i <= data.length; i++) {
+			if (i == 8) { // cookie
+				console.log("cookie");
+			} else if (i == 9) {  //extras menu
+				console.log("extras menu");
+			} else {
+				let coffeeButton = document.getElementById("hstack-item" + i);
+				
+				coffeeButton.style.cursor = 'pointer';
+				coffeeButton.onclick = function () {
+					addCoffee(i)
+				}
+			}
+		}
+	}
+	
+	function setFunc() {
 		data.forEach(function(element, index) { 
 			let itemName = "hstack-item" + index;
-
+			
 			console.log(itemName);
 			
-			if (index > 1 && index != 8) {
+			if (index > 1 && index != 8 && index != 9) {
 				document.getElementById(itemName).innerHTML = `
 				<p class="button-title-small">${element.short_name}</p>
 				<p class="button-sub-small">${element.long_name}</p>
-			`
+				`
+			} else if (index == 8) {
+				document.getElementById(itemName).innerHTML = `
+				<p class="button-title-big">${element.short_name}</p>
+				<p class="button-sub-big">${element.long_name}</p>
+				`
+				
+				
+				itemName = "hstack-item" + (index + 1);
+				document.getElementById(itemName).innerHTML = `
+				<p class="button-sub-big">Other</p>
+				`
+				
 			} else {
 				document.getElementById(itemName).innerHTML = `
 				<p class="button-title-big">${element.short_name}</p>
 				<p class="button-sub-big">${element.long_name}</p>
-			`
+				`
 			}
 		})
-
-
-		// onclick="addCoffee(${index})"
+	}
+	
+	
+	if ( document.readyState == 'complete' ) {
+		setFunc();
+		addClickListener();
 	} else {
-		addEventListener("load", setFunc)
+		addEventListener("load", () => {
+			setFunc();
+			addClickListener();
+		})
 	}
 }
-
-let orderedCoffees = [];
-
-getMenuItems(API_URL + "menu");
 
 function updCurTransList() {
 	var display = '';
 	//var total = 0;
-
+	
 	orderedCoffees.forEach(function(element) { 
 		display += `
-			<p id="order${element.index}" class="order${element.index}" style="background: white; border-radius: 0.4vw; padding: 0.4vw; margin-top: 0.8vw; margin-bottom: 0.8vw;"><span style="font-weight: 900; margin-left: 0.5vw; margin-right: 1.4vw;">${(element.large) ? "L" : "R"}</span>${menu[element.index].long_name}</p>
+		<p id="order${element.index}" class="order${element.index}" style="background: white; border-radius: 0.4vw; padding: 0.4vw; margin-top: 0.8vw; margin-bottom: 0.8vw;"><span style="font-weight: 900; margin-left: 0.5vw; margin-right: 1.4vw;">${(element.large) ? "L" : "R"}</span>${menu[element.index].long_name}</p>
 		`;
 		/*
 		subtotal = menu[element.index].price
@@ -130,142 +270,66 @@ function updCurTransList() {
 				addon = drink_addons[index]
 				switch (addon.type) {
 					case "choice":
-						subtotal += addon.choices[el].cost
-						break;
+					subtotal += addon.choices[el].cost
+					break;
 					case "count":
-						console.log(addon)
-						el.forEach(function(count, idx) {
-							subtotal += (count * addon.choices[idx].cost)
-						})
-						break;
+					console.log(addon)
+					el.forEach(function(count, idx) {
+						subtotal += (count * addon.choices[idx].cost)
+					})
+					break;
 				}
 			})
 		}
 		total += (subtotal * element.count)
 		*/
 	})
-
+	
 	document.getElementById("order-list").innerHTML = display;
 	//document.getElementById("total").innerHTML = total;
 }
 
-var modal
+function endShift() {
+	document.getElementById("endshift-button").addEventListener("click", () => {
+		document.getElementById("endshift-button").style.display = "none";
+		document.getElementById("preorders-button").style.display = "none";
 
-window.addEventListener("load", function(){ modal = document.getElementById("drink-options-modal") })
+		document.getElementById("confirm-endshift-button").style.display = "block";
+		document.getElementById("cancel-endshift-button").style.display = "block";
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-
-
-function postCoffee(coffee) {
-	console.log(coffee)
-	fetch(API_URL + "transactions", {
-		method: "POST",
-		headers: {'Content-Type': 'application/json'},
-		body: JSON.stringify(coffee)
+		console.log("pressed")
 	})
 }
 
+function confirmCancelEnd() {
+	document.getElementById("confirm-endshift-button").addEventListener("click", () => {
+		alert("shift ended!");
+		window.location.href = "./index.html";
+	})
 
+	document.getElementById("cancel-endshift-button").addEventListener("click", () => {
+		document.getElementById("endshift-button").style.display = "block";
+		document.getElementById("preorders-button").style.display = "block";
 
-
-function addCoffee(idx) {
-	item = menu[idx]
-	if (item.is_drink) {
-		modal.style.display = "block"
-		ok = function(iidx) {
-			modal.style.display = 'none'
-			object = {
-				
-
-				index: iidx,
-				addons: [],
-				large: document.getElementById("large").checked,
-				count: 1 // TODO
-			}
-			drink_addons.forEach(function(element, index) {
-				switch (element.type) {
-					case "toggle":
-						object.addons[index] = document.getElementById(element.name).checked
-						break;
-					case "choice":
-						var choice
-						[].slice.call(document.getElementById(element.name).getElementsByTagName("input")).every( (el, idx) => {
-							if (el.checked) {
-								choice = idx
-								return false
-							}
-							return true
-						})
-						object.addons[index] = choice
-						break;
-					case "count":
-						object.addons[index] = []
-						for (idx in element.choices) {
-							object.addons[index][idx] = document.getElementById(`${element.name}choice${idx}`).valueAsNumber
-						}
-
-				}
-			})
-			postCoffee(object);
-
-			orderedCoffees.push(object);
-			updCurTransList()
-		}
-		var options = ""
-		options+="<h3>Large</h3>"
-		options+="<input id=\"large\" type=\"checkbox\">"
-		for (const option of drink_addons) {
-
-			options+=`<h3>${option.name}</h3>`
-			switch (option.type) {
-				case "toggle":
-					options+=`<input id="${option.name}" type=\"checkbox\">`
-					break;
-				case "choice":
-					options+=`<div id="${option.name}">`
-					option.choices.forEach(function(element, index) {
-						options+=`
-						    <input type="radio" id="${option.name}choice${index}"
-								name="${option.name}" ${index === option.default ? "checked" : ""}>
-							<label for="${option.name}choice${index}">${element.name}</label>
-						`
-					})
-					options+="</div>"
-					break;
-				case "count":
-					option.choices.forEach(function(element, index) {
-						options+=`
-							<input type="number" value="0" id="${option.name}choice${index}">
-							<label for="${option.name}choice${index}">${element.name}</label>
-						`
-					})
-			}
-		}
-		modal.innerHTML = `
-			<div class="modal-content">
-				<h2>${item.long_name}</h2>
-				${options}
-				<div class="modal-buttons">
-					<button onclick="modal.style.display = 'none'" type="button">Cancel</button>
-					<button onclick="ok(${idx})" type="button">OK</button>
-				</div>
-			</div>
-		`
-	} else {
-		pushCoffee(item);
-
-		orderedCoffees.push({index: idx});
-		updCurTransList()
-	}
-
+		document.getElementById("confirm-endshift-button").style.display = "none";
+		document.getElementById("cancel-endshift-button").style.display = "none";
+	})
 }
+getMenuItems(API_URL + "menu");
 
 
+window.addEventListener("load", () => {
+	getCurrentDate();
+	modal = document.getElementById("drink-options-modal");
+	document.getElementById("order-sub").innerHTML = getOrderSubText();
+	endShift();
 
+	confirmCancelEnd();
+	
+});
 
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
