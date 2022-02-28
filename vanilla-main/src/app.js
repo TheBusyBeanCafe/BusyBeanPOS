cb4dffd0fd8e22aa2d01e081733aabea441a2cdc
Reagan Bohan <xbjfk.github@gmail.com>
2022-02-28 23:03:03 +1300

    add modification of exitsing orgders

--- vanilla-main/src/app.js
+++ vanilla-main/src/app.js
@@ -54,97 +54,6 @@ function postCoffee(coffee) {
 	})
 }
 
-// function addCoffee(idx) {
-// 	item = menu[idx]
-// 	if (item.is_drink) {
-// 		modal.style.display = "block"
-// 		ok = function(iidx) {
-// 			modal.style.display = 'none'
-// 			object = {
-				
-				
-// 				index: iidx,
-// 				addons: [],
-// 				large: document.getElementById("large").checked,
-// 				count: 1 // TODO
-// 			}
-// 			drink_addons.forEach(function(element, index) {
-// 				switch (element.type) {
-// 					case "toggle":
-// 					object.addons[index] = document.getElementById(element.name).checked
-// 					break;
-// 					case "choice":
-// 					var choice
-// 					[].slice.call(document.getElementById(element.name).getElementsByTagName("input")).every( (el, idx) => {
-// 						if (el.checked) {
-// 							choice = idx
-// 							return false
-// 						}
-// 						return true
-// 					})
-// 					object.addons[index] = choice
-// 					break;
-// 					case "count":
-// 					object.addons[index] = []
-// 					for (idx in element.choices) {
-// 						object.addons[index][idx] = document.getElementById(`${element.name}choice${idx}`).valueAsNumber
-// 					}
-					
-// 				}
-// 			})
-// 			postCoffee(object);
-			
-// 			orderedCoffees.push(object);
-// 			updCurTransList()
-// 		}
-// 		var options = ""
-// 		options+="<h3>Large</h3>"
-// 		options+="<input id=\"large\" type=\"checkbox\">"
-// 		for (const option of drink_addons) {
-			
-// 			options+=`<h3>${option.name}</h3>`
-// 			switch (option.type) {
-// 				case "toggle":
-// 				options+=`<input id="${option.name}" type=\"checkbox\">`
-// 				break;
-// 				case "choice":
-// 				options+=`<div id="${option.name}">`
-// 				option.choices.forEach(function(element, index) {
-// 					options+=`
-// 					<input type="radio" id="${option.name}choice${index}"
-// 					name="${option.name}" ${index === option.default ? "checked" : ""}>
-// 					<label for="${option.name}choice${index}">${element.name}</label>
-// 					`
-// 				})
-// 				options+="</div>"
-// 				break;
-// 				case "count":
-// 				option.choices.forEach(function(element, index) {
-// 					options+=`
-// 					<input type="number" value="0" id="${option.name}choice${index}">
-// 					<label for="${option.name}choice${index}">${element.name}</label>
-// 					`
-// 				})
-// 			}
-// 		}
-// 		modal.innerHTML = `
-// 		<div class="modal-content">
-// 		<h2>${item.long_name}</h2>
-// 		${options}
-// 		<div class="modal-buttons">
-// 		<button onclick="modal.style.display = 'none'" type="button">Cancel</button>
-// 		<button onclick="ok(${idx})" type="button">OK</button>
-// 		</div>
-// 		</div>
-// 		`
-// 	} else {
-// 		pushCoffee(item);
-		
-// 		orderedCoffees.push({index: idx});
-// 		updCurTransList()
-// 	}
-	
-// }
 
 
 function diagButtonClick(elem) {
@@ -157,22 +66,17 @@ function diagButtonClick(elem) {
 }
 
 
-
-
-var displayedDialogIdx;
+var dialogShownObj
 
 
 function doneclicked() {
-	modal.style.display = 'none'
-	object = {
-		id: uuidv4(),
-		index: displayedDialogIdx,
-		count: 1, // TODO also do post
-		date: Date.now(),
-		is_done: false
-		
-	}
-	var choice
+	modal.style.display = 'none';
+	var choice;
+
+	var newobj = dialogShownObj["id"] == null;
+
+	console.log(newobj);
+
 	[].slice.call(document.getElementById("drink-options-content")
 		.querySelectorAll("[data-button-group='milk']"))
 		.every( (el, idx) => {
@@ -183,9 +87,12 @@ function doneclicked() {
 			return true
 		}
 	)
-	object["milk"] = choice;
+	
+	dialogShownObj["date"] = Date.now()
+	
+	dialogShownObj["milk"] = choice;
 
-	object["large"] = document.getElementById("drink-options-content").querySelectorAll("[data-button-group='size']")[1].classList.contains("dialog-button-selected");
+	dialogShownObj["large"] = document.getElementById("drink-options-content").querySelectorAll("[data-button-group='size']")[1].classList.contains("dialog-button-selected");
 
 
 	[].slice.call(document.getElementById("drink-options-content")
@@ -200,19 +107,24 @@ function doneclicked() {
 		});
 
 
-	console.log(object);
 
-	fetch(API_URL + "transactions", {
-		method: "POST",
-		headers: {
-			'Content-Type': 'application/json',
-			'Authorization': 'Basic Q8xTy1zafJmh4R/p9bh11eOcUad/gjoRIeeU214lgtw='
-		},
-		body: JSON.stringify(object)
-	});
+	if (newobj) {
+		dialogShownObj["id"] = uuidv4()
+		orderedCoffees.push(dialogShownObj);
+		console.log(dialogShownObj);
 
+		fetch(API_URL + "transactions", {
+			method: "POST",
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': 'Basic Q8xTy1zafJmh4R/p9bh11eOcUad/gjoRIeeU214lgtw='
+			},
+			body: JSON.stringify(dialogShownObj)
+		});
+	} else {
 
-	orderedCoffees.push(object);
+	}
+
 	updCurTransList()
 }
 
@@ -222,7 +134,12 @@ function coffeeClicked(idx) {
 	item = menu[idx]
 	if (item.is_drink) {
 		modal.style.display = "flex"
-		displayedDialogIdx = idx
+
+		dialogShownObj = {
+			index: idx,
+			count: 1, // TODO also do post
+			is_done: false
+		}
 
 		document.getElementById("dialog-drink-title").innerHTML = getCurrentOrderItem(idx);
     } else {
@@ -359,9 +276,9 @@ function updCurTransList() {
 	}
 
 
-	orderedCoffees.forEach(function(element) { 
+	orderedCoffees.forEach(function(element, index) { 
 		tempBlock = `
-		<p onclick="completeOrder(order${element.index})" id="order${element.index}" class="order-block" style="background: white; border-radius: 0.4vw; padding: 0.4vw; margin-top: 0.8vw; margin-bottom: 0.8vw;"><span style="font-weight: 900; margin-left: 0.5vw; margin-right: 1.4vw;">${(element.large) ? "L" : "R"}</span>${menu[element.index].long_name}
+		<p onclick="completeOrder(${index})" id="order${index}" class="order-block" style="background: white; border-radius: 0.4vw; padding: 0.4vw; margin-top: 0.8vw; margin-bottom: 0.8vw;"><span style="font-weight: 900; margin-left: 0.5vw; margin-right: 1.4vw;">${(element.large) ? "L" : "R"}</span>${menu[element.index].long_name}
 		`;
 		
 		
@@ -391,10 +308,12 @@ function updCurTransList() {
 */
 }
 
-function completeOrder(el) {
-	
-	console.log();
+function completeOrder(idx) {
+	modal.style.display = "flex"
 
+	dialogShownObj = orderedCoffees[idx];
+	
+	/*
 	if (el.is_done) {
 		el.style.background="white";
 	} else {
@@ -403,6 +322,7 @@ function completeOrder(el) {
 			el.style.background="#A5D6A7";
 		}
 	}
+	*/
 }
 
 
